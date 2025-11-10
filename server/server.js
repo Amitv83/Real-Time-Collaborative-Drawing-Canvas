c2a89289107e79ca4a3adde49a64631466daeac4
AmitKumar Vishwakarma <amitv8328@gmail.com>
2025-11-10 20:22:41 +0530

    cors for frontend url fixed

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const server = createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://real-time-collaborative-drawing-can-chi.vercel.app/",
+    origin: "https://real-time-collaborative-drawing-can-chi.vercel.app",
     methods: ["GET", "POST"]
   }
 });
